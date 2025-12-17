63f654730f699792bc6b939d7748ae119e4f7a59
abjin <abjin69@gmail.com>
2025-12-17 15:17:05 +0900

    exposes memory usage statistics

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,37 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getMemoryStatus() {
+    const memoryUsage = process.memoryUsage();
+    const formatMemory = (bytes: number) =>
+      parseFloat((bytes / 1024 / 1024).toFixed(2));
+
+    return {
+      rss: {
+        value: formatMemory(memoryUsage.rss),
+        unit: 'MB',
+        description: 'Total memory allocated',
+      },
+      heapTotal: {
+        value: formatMemory(memoryUsage.heapTotal),
+        unit: 'MB',
+        description: 'Heap allocated',
+      },
+      heapUsed: {
+        value: formatMemory(memoryUsage.heapUsed),
+        unit: 'MB',
+        description: 'Heap used',
+      },
+      external: {
+        value: formatMemory(memoryUsage.external),
+        unit: 'MB',
+        description: 'C++ objects memory',
+      },
+      arrayBuffers: {
+        value: formatMemory(memoryUsage.arrayBuffers),
+        unit: 'MB',
+        description: 'Array Buffers memory',
+      },
+    };
   }
 }
