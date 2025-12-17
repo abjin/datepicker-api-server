34583add1a3674bf3f8e19d1079140f8fd3ff4a1
abjin <abjin69@gmail.com>
2025-12-17 23:17:20 +0900

    add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthModule } from './modules/auth/auth.module';
 import { PrismaModule } from '../libs/prisma/src';
 import { ConfigModule } from '@nestjs/config';
 import { BearerStrategy } from './common/strategy';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { BearerStrategy } from './common/strategy';
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule.forRoot({ isGlobal: true }),
     AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService, BearerStrategy],
--- src/common/guard.ts
+++ src/common/guard.ts
@@ -1,11 +1,5 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class BearerGuard extends AuthGuard(['bearer']) {
-  canActivate(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest();
-    if (request.user) return true;
-    return super.canActivate(context);
-  }
-}
+export class BearerGuard extends AuthGuard(['bearer']) {}
