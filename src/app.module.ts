2933f7535cedff53982244f541ecb5e721e3819a
abjin <abjin69@gmail.com>
2025-12-17 14:53:35 +0900

    ad http module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [{ ...HttpModule.register({}), global: true }, AuthModule],
   controllers: [AppController],
   providers: [AppService],
+  exports: [HttpModule],
 })
 export class AppModule {}
