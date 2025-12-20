d73405f7845094c550b6b86e373d300c8ffbd207
abjin <abjin69@gmail.com>
2025-12-20 13:20:29 +0900

    add cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://date-picker-share-web.vercel.app',
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   });
